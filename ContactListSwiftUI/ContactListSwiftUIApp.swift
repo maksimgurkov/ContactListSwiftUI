b785e0a70b371c71b8b4ac55cdb031e6cf23aa0f
Maksim Gurkov <maksim.gurkov@gmail.com>
2023-05-04 10:03:36 +0300

    Initial Commit

---
+++ ContactListSwiftUI/ContactListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSwiftUIApp.swift
+//  ContactListSwiftUI
+//
+//  Created by Максим Гурков on 04.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
